8a94a1e87bea4b8c14d7a7404a791e7087ccee9d
YSendhav <88848775+YSendhav@users.noreply.github.com>
2024-03-13 07:32:32 +0530

    first commit

---
+++ ArthmaticOperations/ArthmaticOperations.h
@@ -0,0 +1,18 @@
+//
+//  ArthmaticOperations.h
+//  ArthmaticOperations
+//
+//  Created by YOGENDRA_SENDHAV on 13/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArthmaticOperations.
+FOUNDATION_EXPORT double ArthmaticOperationsVersionNumber;
+
+//! Project version string for ArthmaticOperations.
+FOUNDATION_EXPORT const unsigned char ArthmaticOperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArthmaticOperations/PublicHeader.h>
+
+
---
+++ SumOfTwoNumbers.swift
@@ -0,0 +1,33 @@
+//
+//  SumOfTwoNumbers.swift
+//  ArthmaticOperations
+//
+//  Created by YOGENDRA_SENDHAV on 13/03/24.
+//
+
+import Foundation
+import UIKit
+
+///SUM OF TWO NUMBERS
+public func SumOfTwoNumbers(num1: Int, num2: Int)-> Int{
+   return num1 + num2
+}
+
+///SUBSTRACTION OF TWO NUMBERS
+public func SubtractionOfTwoNumbers(num1: Int, num2: Int)-> Int{
+   return num1 - num2
+}
+
+///MULTIPLY OF TWO NUMBERS
+public func MultiplyOfTwoNumbers(num1: Int, num2: Int)-> Int{
+   return num1 * num2
+}
+
+///SIMPLE INTEREST
+public func SimpleInterest(num1: Int, num2: Int, num3: Int)-> Int{
+   return (num1 * num2 * num3)/100
+}
+
+
+
+
